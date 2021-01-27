03b4a4e5257d3b63f62c896ed3bdf9b7a7bd245f
Filadelfo <filadelfo.braz@gmail.com>
2021-01-27 11:21:06 +0100

    Style: Run format script

--- react/BindingSelector.tsx
+++ react/BindingSelector.tsx
@@ -1,22 +1,20 @@
-import React, { FC } from 'react'
-import { FormattedMessage } from 'react-intl'
-import { Layout, PageBlock, PageHeader } from 'vtex.styleguide'
+import type { FC } from "react";
+import React from "react";
+import { FormattedMessage } from "react-intl";
+import { Layout, PageBlock, PageHeader } from "vtex.styleguide";
 
-import './styles.global.css'
+import "./styles.global.css";
 
 const BindingSelector: FC = () => {
   return (
     <Layout
       pageHeader={
-        <PageHeader
-          title={<FormattedMessage id="admin-binding-selector" />}
-        />
+        <PageHeader title={<FormattedMessage id="admin-binding-selector" />} />
       }
     >
-      <PageBlock variation="full">
-      </PageBlock>
+      <PageBlock variation="full" />
     </Layout>
-  )
-}
+  );
+};
 
-export default BindingSelector
+export default BindingSelector;
