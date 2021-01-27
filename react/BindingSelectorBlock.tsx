a4db17c552f591f6a088125b989a7ba74f7812db
Filadelfo <filadelfo.braz@gmail.com>
2021-01-27 14:13:34 +0100

    Feat: Add store builder to make app usable in store theme

---
+++ react/BindingSelectorBlock.tsx
@@ -0,0 +1,8 @@
+import type { FC } from "react";
+import React from "react";
+
+const BindingSelectorBlock: FC = () => {
+  return <div>Binding Selector</div>;
+};
+
+export default BindingSelectorBlock;
