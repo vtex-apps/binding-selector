b7b5f3d8885784463a8e5387ca8d139fcaebdb6c
Filadelfo <filadelfo.braz@gmail.com>
2021-01-28 15:23:46 +0100

    Feat: Add dropdown menu with a list of options

--- react/BindingSelectorBlock.tsx
+++ react/BindingSelectorBlock.tsx
@@ -1,8 +1,91 @@
-import type { FC } from "react";
-import React from "react";
+import type { FC } from 'react'
+import React, { useState } from 'react'
+import { useCssHandles } from 'vtex.css-handles'
+
+const fakeBindingsLabel: {
+  [Identifier: string]: { [Identifier: string]: string }
+} = {
+  'pt-BR': {
+    'pt-BR': 'Brasil',
+    'it-IT': 'Itália',
+    'en-US': 'EUA',
+  },
+  'it-IT': {
+    'pt-BR': 'Brasile',
+    'it-IT': 'Italia',
+    'en-US': 'USA',
+  },
+  'en-US': {
+    'pt-BR': 'Brazil',
+    'it-IT': 'Italy',
+    'en-US': 'US',
+  },
+}
+
+const CSS_HANDLES = [
+  'container',
+  'relativeContainer',
+  'button',
+  'buttonTextClasses',
+  'list',
+  'listElement',
+] as const
 
 const BindingSelectorBlock: FC = () => {
-  return <div>Binding Selector</div>;
-};
+  const [currentBinding, setCurrentBiding] = useState<string>('pt-BR')
+  const [open, setOpen] = useState<boolean>(false)
+  const handles = useCssHandles(CSS_HANDLES)
 
-export default BindingSelectorBlock;
+  const handleClick = () => {
+    setOpen(!open)
+  }
+
+  return (
+    <div
+      className={`${handles.container} flex items-center justify-center w3 relative`}
+    >
+      <div
+        className={`${handles.relativeContainer} relative flex justify-center`}
+      >
+        <button
+          type="button"
+          onClick={handleClick}
+          className={`${handles.button} link pa3 bg-transparent bn flex items-center pointer c-on-base`}
+        >
+          <span className={`${handles.buttonTextClasses}`}>
+            {fakeBindingsLabel[currentBinding][currentBinding]}
+          </span>
+        </button>
+        <ul
+          hidden={!open}
+          className={`${handles.list} absolute z-5 list top-1 ph0 mh0 mt5 bg-base`}
+        >
+          {Object.keys(fakeBindingsLabel[currentBinding])
+            .filter((binding) => {
+              return binding !== currentBinding
+            })
+            .map((binding) => (
+              // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
+              <li
+                key={binding}
+                className={`${handles.listElement} t-action--small pointer f5 pa3 hover-bg-muted-5 tc`}
+                onClick={() => {
+                  setCurrentBiding(binding)
+                  setOpen(false)
+                }}
+                onKeyDown={() => {
+                  setCurrentBiding(binding)
+                  setOpen(false)
+                }}
+                onMouseDown={(e) => e.preventDefault()}
+              >
+                <span>{fakeBindingsLabel[currentBinding][binding]}</span>
+              </li>
+            ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default BindingSelectorBlock
