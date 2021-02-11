2037f216dd9cb68bbfefec59bfeabc0370236383
Filadelfo <filadelfo.braz@gmail.com>
2021-02-11 21:11:35 +0100

    Chore: Add graphql mutation to updateSalesChannel

--- node/resolvers/orderForm.ts
+++ node/resolvers/orderForm.ts
@@ -3,19 +3,19 @@ import type { OrderForm as CheckoutOrderForm } from 'vtex.checkout-graphql'
 export const updateSalesChannel = (
   _: unknown,
   args: {
-    orderForm: string
+    orderFormId: string
     salesChannel: string
     clientPreferencesData: CheckoutOrderForm['clientPreferencesData']
   },
   ctx: Context
 ): Promise<CheckoutOrderForm> => {
   const { clients } = ctx
-  const { orderForm, salesChannel, clientPreferencesData } = args
+  const { orderFormId, salesChannel, clientPreferencesData } = args
 
   const { checkout } = clients
 
   return checkout.updateSalesChannel(
-    orderForm,
+    orderFormId,
     salesChannel,
     clientPreferencesData
   )
